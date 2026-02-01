4003676cd26a61feae6cfe95d979029dcf42c1d5
Wallace Gabriel <gs008963@gmail.com>
2026-01-31 21:37:44 -0300

    Initial Commit

---
+++ backend/mmo-social-platform/src/main/java/com/ifpe/mmo_social_platform/MmoSocialPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ifpe.mmo_social_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmoSocialPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmoSocialPlatformApplication.class, args);
+	}
+
+}
---
+++ backend/mmo-social-platform/src/test/java/com/ifpe/mmo_social_platform/MmoSocialPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifpe.mmo_social_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmoSocialPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
