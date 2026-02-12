65e05640d9b69542ccceebd3db2a9edba6f9960e
Wallace Gabriel <gs008963@gmail.com>
2026-02-12 00:44:16 +0000

    feat: adicionar validação para e-mail obrigatório

--- backend/mmo-social-platform/src/main/java/com/ifpe/mmo_social_platform/dto/auth/register/UserRequestDto.java
+++ backend/mmo-social-platform/src/main/java/com/ifpe/mmo_social_platform/dto/auth/register/UserRequestDto.java
@@ -8,6 +8,7 @@ public class UserRequestDto {
   @NotBlank(message = "Username is required")
   private String username;
 
+  @NotBlank(message = "Email is required")
   @Email(message = "Email should be valid")
   private String email;
 
